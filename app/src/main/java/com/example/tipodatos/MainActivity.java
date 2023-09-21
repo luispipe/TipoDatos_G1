2883fc9b891460c76bead723f9c5015a35745b2b
luispipe <lflondonor@unal.edu.co>
2023-09-21 10:41:07 -0300

    second commit

--- app/src/main/java/com/example/tipodatos/MainActivity.java
+++ app/src/main/java/com/example/tipodatos/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.tipodatos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button botonAscii= findViewById(R.id.buttonASCII);
+        Button botonPotencia= findViewById(R.id.buttonCalcular);
+        Button botonMatchText= findViewById(R.id.buttonMatchText);
+
+        Intent ascii= new Intent(getApplicationContext(), AsciiActivity.class);
+        Intent potencia= new Intent(getApplicationContext(), PotenciaActivity.class);
+        Intent matchText= new Intent(getApplicationContext(), MainActivity.class);
+
+        botonAscii.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(ascii);
+            }
+        });
+
+        botonPotencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(potencia);
+            }
+        });
+
+        botonMatchText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(matchText);
+            }
+        });
+
         /*Tipos de datos
-        * Nativos
+        * Nativos java
         * Númerico - Texto - Booleano
         * Númericos Enteros: byte-short-int (comun)-long
         * Númericos Decimales: float - double (comun)
@@ -22,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         * */
         //declaración x,y,z
         //int x,y,z;
-        int x;
+      /*  int x;
         int y;
         int z;
         x=15;//defini x
@@ -33,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         short ejemploShort= 25000;
         int ejemploInt= 999999999;
         long ejemploLong= 1000000000000000000l;
+
         float ejemploFloat= 1.555f;
         double ejemploDouble= 1.5555;
 
@@ -41,9 +74,11 @@ public class MainActivity extends AppCompatActivity {
         boolean ejemploBoolean= true;
 
         //print permite mostrar cualquier tipo de dato
+        //println es la forma de imprimir de java
         System.out.println(ejemploTexto);
         //Log solo permite mostrar texto
-        Log.i("String",ejemploByte+"");
+        //Log la forma de imprimir de android
+        Log.i("Casteo de byte a String",ejemploByte+"");
 
         //Conversión de tipos de datos o Casteo
         // De cualquier tipo de dato a String
@@ -56,16 +91,27 @@ public class MainActivity extends AppCompatActivity {
         String casteoChar= ejemploChar+"";
         String casteoChar2= String.valueOf(ejemploChar);
 
-        //De cualqueir tipo de dato a int
+        //De cualquier tipo de dato a int
         int casteoDouble= (int)ejemploDouble;//Que tomo la parte entera del número ej: 5.68 tomo 5
         int casteoString= Integer.parseInt(ejemploTexto);//Solo funciona si hay un número entre comillas o el texto es un número
         int casteoCharInt= (int)ejemploChar;
 
+        System.out.println((int)5.29);
+
+        //Casteo para convertir en decimal
+        double casteoInt_Double= 33; // 33 --> 33.0
+        double casteoString_double= Double.parseDouble("55.25");//Si dentro de las comillas hay valores diferentes a númericos da error
+
+        //Casteo de un entero a caracter
+        int letra_C= 67;
+        char c= (char) letra_C;
+
+        /*
+           Ejemplo ASCII
         char letra_a= 'a';
         char letra_A= 'A';
-
         System.out.println((int)letra_A);
         System.out.println((int)letra_a);
-        System.out.println(casteoCharInt);
+        System.out.println(casteoCharInt);*/
     }
 }
